0cb40b10d42c96d50229ad2bdc73510cc5d0cbc6
Kalaskar <2166843@cognizant.com>
2022-06-24 12:19:38 +0530

    files added

---
+++ src/main/java/com/example/basicdemo/BasicDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.basicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDemoApplication.class, args);
+		System.out.println("Dummy application for Jenkins trial");
+	}
+
+}
